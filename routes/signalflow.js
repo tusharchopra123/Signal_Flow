ecb28f361f15f5de4336ea5415520c3547fcde20
tusharchopra123 <tusharchopra123@gmail.com>
2020-09-09 21:05:14 +0530

    changes

--- routes/signalflow.js
+++ routes/signalflow.js
@@ -40,6 +40,24 @@ route.get('/style2', (req, res) => {
   res.sendFile(path.join(__dirname, '../css/style2.css'))
 
 })
+route.get('/js/jquery',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../js/jquery-3.3.1.slim.min.js'));
+})
+route.get('/js/popper',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../js/popper.min.js'));
+})
+route.get('/js/bootstrap',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../js/bootstrap.min.js'));
+})
+route.get('/js/SignalFlowGraph',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../js/SignalFlowGraph.js'));
+})
+route.get('/js/guiUtilities',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../js/guiUtilities.js'));
+})
+route.get('/js/gui',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../js/gui.js'));
+})
 route.get('/api/dash', authCheck, (req, res) => {
   res.status(200).send([10,2,9,1]);
 })
